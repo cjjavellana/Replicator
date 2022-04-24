f54d810c636c9e925fe985500d8889396aa46826
Christian Javellana <cjjavellana@gmail.com>
2022-04-24 19:45:55 +0800

    (fix) Syntax error

--- reset-mongodb.sh
+++ reset-mongodb.sh
@@ -4,5 +4,5 @@ rm -rf ./data/mongodb
 mkdir ./data/mongodb
 touch ./data/mongodb/.keep
 
-docker rm "$(docker ps -aq --filter="status=exited")"
+docker rm $(docker ps -aq --filter="status=exited")
 docker volume prune
