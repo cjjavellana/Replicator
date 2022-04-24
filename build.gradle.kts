620bd2ced68911c53d70204729f9144d9da86550
Christian Javellana <cjjavellana@gmail.com>
2022-04-24 16:33:41 +0800

    (chore) Various dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,59 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.7"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.6.7"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "io.cjavellana"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+val debeziumVersion = "1.9.1.Final"
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // replicator's own storage for deduplication
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
+
+    // debezium
+    implementation("io.debezium:debezium-api:$debeziumVersion")
+    implementation("io.debezium:debezium-embedded:$debeziumVersion")
+    implementation("io.debezium:debezium-server-redis:$debeziumVersion")
+
+    implementation("io.debezium:debezium-connector-mysql:$debeziumVersion")
+    implementation("io.debezium:debezium-connector-mongodb:$debeziumVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
